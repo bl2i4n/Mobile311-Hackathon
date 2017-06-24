7ac273eb1e28990279656131ea7f6a594a6bd055
bl2i4n <bl2i4n@yahoo.com>
2017-06-23 21:55:41 -0700

    added hot loader to see changes instantly , was running into issue with different versions of webpack

--- mobile311/webpack.config.dev.js
+++ mobile311/webpack.config.dev.js
@@ -1,16 +1,27 @@
 import path from 'path'
+import webpack from 'webpack';
 
 export default {
-  entry: path.join(__dirname, '/client/index.js'),
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
   output: {
-    path: '/'
+    path: '/',
+    publicPath: '/'
   },
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader'] //will transpile es6 code
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader']
       }
     ]
   },
