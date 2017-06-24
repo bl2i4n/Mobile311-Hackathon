738aa18493784767c73beb17d587b7c9a6655064
bl2i4n <bl2i4n@yahoo.com>
2017-06-23 22:14:18 -0700

    debugging react router

---
+++ mobile311/client/components/NavigationBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => {
+
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand">Home</a>
+          </div>
+
+          <div className="collapse navbar-collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li><a href="#">Sign up</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+
+
+}
