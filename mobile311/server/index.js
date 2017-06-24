cd91efb888956e1e5cfaa9fe5c7760b15b054676
bl2i4n <bl2i4n@yahoo.com>
2017-06-23 20:49:42 -0700

    adding express.js for local hosting and tested

---
+++ mobile311/server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import path from 'path';
+
+let app = express();
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(3000, () => console.log('Running in localhost:3000'));
