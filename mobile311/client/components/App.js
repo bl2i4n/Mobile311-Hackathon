522207c1d5f438d087cb27bd70d5ef072b9a1451
bl2i4n <bl2i4n@yahoo.com>
2017-06-23 21:19:23 -0700

    added babel and react, also had to install older version of webpack for auth

--- mobile311/client/components/App.js
+++ mobile311/client/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export deault () => {
+export default () => {
   return (
     <h1>Hello from react</h1>
   );
--- mobile311/webpack.config.dev.js
+++ mobile311/webpack.config.dev.js
@@ -1,7 +1,20 @@
+import path from 'path'
+
 export default {
   entry: path.join(__dirname, '/client/index.js'),
   output: {
     path: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'client'),
+        loaders: ['babel-loader'] //will transpile es6 code
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
   }
-
 }
