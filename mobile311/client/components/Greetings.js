b9e1c4040eca94099140e51dca9de7d4e864ac1e
bl2i4n <bl2i4n@yahoo.com>
2017-06-23 23:33:39 -0700

    added state for forms, need database for users

--- mobile311/client/components/Greetings.js
+++ mobile311/client/components/Greetings.js
@@ -4,7 +4,7 @@ class Greetings extends React.Component{
   render (){
     return(
       <div className="jumbotron">
-        <h1>Hi!!</h1>
+        <h1>Center Banner</h1>
       </div>
     );
   }
---
+++ mobile311/client/components/signup/SignupForm.js
@@ -0,0 +1,92 @@
+import React from 'react';
+
+class SignupForm extends React.Component{
+  constructor(props){
+      super(props);
+      this.state = {
+        username: '',
+        email: '',
+        password: '',
+        state: '',
+        phone: '',
+      }
+
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+
+  }
+
+  onChange(event){
+    this.setState({ [event.target.name]: event.target.value});
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render (){
+    return(
+      <form onSubmit={this.onSubmit}>
+        <h1>Join our community!</h1>
+
+        <div className="form-group">
+          <label className="control-label">Name</label>
+          <input
+            value ={this.state.username}
+            onChange={this.onChange}
+            type="text"
+            name="username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Email</label>
+          <input
+            value ={this.state.email}
+            onChange={this.onChange}
+            type="text"
+            name="email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">Password</label>
+          <input
+            value ={this.state.password}
+            onChange={this.onChange}
+            type="password"
+            name="password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="form-group">
+          <label className="control-label">State</label>
+          <input
+            value ={this.state.state}
+            onChange={this.onChange}
+            type="text"
+            name="state"
+            className="form-control"
+          />
+        </div>
+
+
+        <div className="form-group">
+          <button className="btn btn-primary btn-lg">
+            Sign Up
+          </button>
+        </div>
+
+      </form>
+
+    );
+  }
+
+}
+
+
+export default SignupForm;
