d793c0049198620ba9de581fa728eb4d1ac33edd
bl2i4n <bl2i4n@yahoo.com>
2017-06-25 01:50:37 -0700

    added font awesome and tachyon styling for main hero page

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -3,8 +3,15 @@ import React from 'react';
 class Greetings extends React.Component{
   render (){
     return(
-      <div className="jumbotron">
-        <h1>Center Banner</h1>
+      <div id="splash" className="jumbotron flx-1 centered">
+        <h1 className="jcc asc">Mobile 311</h1>
+          <div id="icons">
+            <i className="fa fa-bullhorn fa-2x ma3"></i>
+            <i className="fa fa-plus fa-2x ma3"></i>
+            <i className="fa fa-life-ring fa-2x ma3"></i>
+          </div>
+        <h2>Improving City Services</h2>
+
       </div>
     );
   }
