fab04ae9b30177839905b1f8a0168958757cfb5e
bl2i4n <bl2i4n@yahoo.com>
2017-06-25 12:52:31 -0700

    fixed coloring of navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,15 +7,15 @@ export default () => {
       <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
-            <Link to="/" className="navbar-brand">Mobile 311</Link>
+            <Link to="/" className="navbar-brand" id="ul-main">Mobile 311</Link>
           </div>
 
           <div className="collapse navbar-collapse">
             <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/signup">Sign up</Link></li>
+              <li><Link to="/signup" id="ul-main">Sign up</Link></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/login">Log In</Link></li>
+              <li><Link to="/login" id="ul-main">Log In</Link></li>
             </ul>
           </div>
         </div>
