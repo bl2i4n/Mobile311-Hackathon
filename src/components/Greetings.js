cd8a5cd20a79729235227bf3481789be672f36c2
bl2i4n <bl2i4n@yahoo.com>
2017-06-25 12:26:44 -0700

    adding styling for main page

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -3,14 +3,14 @@ import React from 'react';
 class Greetings extends React.Component{
   render (){
     return(
-      <div id="splash" className="jumbotron flx-1 centered">
-        <h1 className="jcc asc">Mobile 311</h1>
-          <div id="icons">
+      <div id="splash" className="splash flx-1 centered">
+        <h1 className="jcc asc h1-main">Mobile 311</h1>
+          <div id="icons" className="i-main">
             <i className="fa fa-bullhorn fa-2x ma3"></i>
             <i className="fa fa-plus fa-2x ma3"></i>
             <i className="fa fa-life-ring fa-2x ma3"></i>
           </div>
-        <h2>Improving City Services</h2>
+        <h2 className="h2-main">Improving City Services</h2>
 
       </div>
     );
